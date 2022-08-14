0a4bf03117abb2572bda9852a855e8736738e3cc
pratikdevre <68334102+pratikdevre@users.noreply.github.com>
2022-08-15 01:56:48 +0530

    APIs Added

--- src/main/java/com/pgrms/PgRentalServices/repositories/ConfirmedRepository.java
+++ src/main/java/com/pgrms/PgRentalServices/repositories/ConfirmedRepository.java
@@ -3,9 +3,9 @@ package com.pgrms.PgRentalServices.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pgrms.PgRentalServices.Entities.Users;
+import com.pgrms.PgRentalServices.Entities.Confirmed;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Integer>{
+public interface ConfirmedRepository extends JpaRepository<Confirmed, Integer> {
 
 }
--- src/main/java/com/pgrms/PgRentalServices/repositories/ReviewsRepository.java
+++ src/main/java/com/pgrms/PgRentalServices/repositories/ReviewsRepository.java
@@ -3,9 +3,8 @@ package com.pgrms.PgRentalServices.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pgrms.PgRentalServices.Entities.Users;
-
+import com.pgrms.PgRentalServices.Entities.Reviews;
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Integer>{
-
+public interface ReviewsRepository extends JpaRepository<Reviews, Integer>{
+	
 }
